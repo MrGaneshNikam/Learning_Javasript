da4c6f99721db5a93dfc5a60ab60f389143928a5
Ganesh nikam67 <nikamganesh7083@gmail.com>
2025-11-29 10:24:46 +0530

    switch  statements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
-console.log("hello world");
-console.log("ganesh here");
-let a = 10;
-let b = 5;
-console.log("sum is :",a+b);
+// console.log("hello world");
+// console.log("ganesh here");
+// let a = 10;
+// let b = 5;
+// console.log("sum is :",a+b);
+
+let color = "green";
+
+switch (color) {
+    case "red" :
+
+console.log("stop")
+break;
+
+    case "yellow" :
+        console.log("go slow")
+break;
+
+    case "green" :
+        console.log("go")
+break;
+
+default :
+        console.log("broken light");
+}
+
+let day ="6";
+switch (day) {
+    case "1" :
+        console.log("monday");
+        break;
+    case "2" :
+        console.log("tuesday");
+           break;
+    case "3" :
+        console.log("wednsday");
+                break;
+     case "4" :
+        console.log("thursday");
+        break;
+         case "5" :
+        console.log("friday");
+        break;
+         case "6" :
+        console.log("saturday");
+        break;
+         case "7" :
+        console.log("sunday");
+        break;
+        default :
+        console.log("its weekday");
+
+}
